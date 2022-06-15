fb1d9dff496cc523c14e5466246f5b3f252c779b
cryoung1998 <cryoung@g.clemson.edu>
2022-06-15 10:54:41 -1000

    Added may 25

--- resources/scripts/dateInformation.js
+++ resources/scripts/dateInformation.js
@@ -11,7 +11,8 @@ const dataFile = {
             "21":"https://lh3.googleusercontent.com/pw/AM-JKLU482BXPOWYzL9EPxsrnfNHAJzNDA26cQdb53sSgwIo7TN5Tbp-qBazEoTBTscNn8hVFG_NA7dTlyiXij8ScKXRy41q0XkxyyqKzYOpm5BB6IY_DEEWlbgPCjXIiXe5anzNncrD5V93LOyWukX2yEmF=w1218-h914-no?authuser=0",
             "22":"https://lh3.googleusercontent.com/pw/AM-JKLVacHPaFUKROmOY8ATAOlJGZPoFi5uMzYPLFPqTV7Iw9uicWB5E7AMTGa-OPDkodtS87KIE2LuYQMWmhyXdTJJtESO4QkqLInvTB_gBQDPJmroRCfNIc_iQbX8AvYAJSP6J56fnty7nohU6SYHJ4n3x=w419-h558-no?authuser=0",
             "23":"https://lh3.googleusercontent.com/pw/AM-JKLU_QzdSBBTTapL-btxCVrxOYEtUkc9G9CeEz8NI_P5NXD7K6Uo-vJjEfE91PO_vBAIpAo1zMu644P70dnUdJD1TPNsTLUvoTtztD1YQg1VC13azswNmdipO1_-MCGpIgNS0MhhiXBJ-2Cxg9JExNZte=w686-h914-no?authuser=0",
-            "24":"https://lh3.googleusercontent.com/pw/AM-JKLXGZIdO5gUYVmWIdqDgBI6t-Q7m4bYkr89_4C7HucaIz4ru-qD2E5M_DzQYTjbveKM9SP7_5PuvsZx-X0D7m3jLe3rwYGoANsF20GG31gOK7UZyi-SWobVh7YAmPUaj-R4A8BVpYc4iNXwQHc9DDXNP=w1218-h914-no?authuser=0"
+            "24":"https://lh3.googleusercontent.com/pw/AM-JKLXGZIdO5gUYVmWIdqDgBI6t-Q7m4bYkr89_4C7HucaIz4ru-qD2E5M_DzQYTjbveKM9SP7_5PuvsZx-X0D7m3jLe3rwYGoANsF20GG31gOK7UZyi-SWobVh7YAmPUaj-R4A8BVpYc4iNXwQHc9DDXNP=w1218-h914-no?authuser=0",
+            "25":"https://lh3.googleusercontent.com/pw/AM-JKLVQXCYHijT_r7iBG5fOHwIY1CrGGM3AIyytQqMvSeO8b_VI_nf_E2RRE81Altx3rcmS9R9Lx3G7xQC-qBGWzjuIQ4PCopYuw90W5YjBrEsfXOrPA9O58g5QB7yrmKYLV02s-rt81nb5YeM6-OAU5uH8=w1052-h789-no?authuser=0"
         }
     }
 };
